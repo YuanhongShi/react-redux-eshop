b2856a59a074b661d9b286bf4667dec800c745c1
Yuanhong Shi <shiyuanhong@gmail.com>
2021-09-07 21:31:57 -0700

    update the following content

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
-import { combineReducers } from "redux";
-import { productReducer } from "./productReducer";
+import { combineReducers } from 'redux'
+import { productReducer } from './productReducer'
 
 const reducers = combineReducers({
-  allProducts: productReducer,
+    allProducts: productReducer,
 })
+
+export default reducers
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,18 +1,14 @@
-import { ActionTypes } from "../constants/action-types";
+import { ActionTypes } from '../constants/action-types'
 
 const initialState = {
-  products: [{
-    id: 1,
-    title: "Dipesh",
-    category: "programmer"
-  }],
-};
+    products: [],
+}
 
-export const productReducer = (state, { type, payload }) => {
-  switch (type) {
-    case ActionTypes.SET_PRODUCTS:
-      return state;
-    default:
-      return state;
-  }
-};
+export const productReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return { ...state, products: payload }
+        default:
+            return state
+    }
+}
