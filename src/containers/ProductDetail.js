dde499c8145a1ac084b56ed45ce1d2c20a95ade6
Yuanhong Shi <shiyuanhong@gmail.com>
2021-09-09 20:00:44 -0700

    update reducer

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectedProduct } from '../redux/actions/productActions'
 
 const ProductDetail = () => {
+    const product = useSelector((state) => state.product)
+    const { productId } = useParams()
+    const dispatch = useDispatch()
+    console.log(product)
+
+    const fetchProductDetail = async () => {
+        const response = await axios
+            .get(`https://fakestoreapi.com/products/${productId}`)
+            .catch((err) => {
+                console.log('Err ', err)
+            })
+
+        dispatch(selectedProduct(response.data))
+    }
+    useEffect(() => {
+        if (productId && productId !== '') fetchProductDetail()
+    }, [productId])
+
     return (
-        <div>
-            <h1>ProductDetail</h1>
+        <div className='ui grid container'>
+            <div className='ui placeholder segment'></div>
         </div>
     )
 }
